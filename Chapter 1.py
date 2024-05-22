290ba3184775b9d2254f11555416e938ace46d42
Asad <syedzadaaa42@gmail.com>
2024-05-22 13:34:41 +0100

    updated the def addition function

--- Chapter 1.py
+++ Chapter 1.py
@@ -3,7 +3,13 @@ print("Asad")
 
 
 def addition(x, y):
-    return x + y
+    y = int(x+y)
+    return y
 
 
-user = input("")
+user = int(input("please Input first Number : "))
+user2 = int(input("Please input Second Number : "))
+
+result= addition(user, user2)
+
+print(f"The addition of {user} and {user2} is : {result}")
